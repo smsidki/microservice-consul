9075e33155d56ce43fb07c2e6a121998132f5873
smsidki <sichy_helsinky@yahoo.com>
2017-10-02 09:18:23 +0700

    change properties for consul

--- book-service/src/main/java/com/greenfoarfece/ms/book/BookApplication.java
+++ book-service/src/main/java/com/greenfoarfece/ms/book/BookApplication.java
@@ -2,12 +2,12 @@ package com.greenfoarfece.ms.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BookApplication {
-	
+
 	public static void main(String... args) {
 		SpringApplication.run(BookApplication.class, args);
 	}
